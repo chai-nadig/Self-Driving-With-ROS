b9a3579e881e7b9871aa326045783b1f017577d1
chai-nadig <chaitanya.nadig@gmail.com>
2019-08-04 14:49:44 -0700

    fix line

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -88,7 +88,7 @@ class WaypointUpdater(object):
         self.base_waypoints = waypoints
         if not self.waypoints_2d:
             self.waypoints_2d = [
-                [waypoint.pose.pose.position.x, waypoints.pose.pose.position.y] for waypoint in waypoints.waypoints
+                [waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints
             ]
             self.waypoints_tree = KDTree(self.waypoints_2d)
 
