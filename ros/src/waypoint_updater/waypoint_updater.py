f6f9adb24e6eea126d8f014e730fd4359b927730
chai-nadig <chaitanya.nadig@gmail.com>
2019-08-05 00:52:41 -0700

    logging

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -102,7 +102,7 @@ class WaypointUpdater(object):
         if self.stopline_wp_idx == -1 or self.stopline_wp_idx >= farthest_idx:
             lane.waypoints = base_waypoints
         else:
-            rospy.loginf('Decelerating - stopline_wp_idx {}, farthest_idx {}'.format(
+            rospy.loginfo('Decelerating - stopline_wp_idx {}, farthest_idx {}'.format(
                 self.stopline_wp_idx, farthest_idx))
             lane.waypoints = self.decelerate_waypoints(base_waypoints, closest_idx)
 
